583f35f581429674e30b4c4d116653231aeed853
tomek <tomek.p93@gmail.com>
2018-07-14 20:23:52 +0200

    Add basic_code_pattern_generator

---
+++ include/mastermind_basic_code_pattern_generator.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <algorithm>
+#include <random>
+#include <vector>
+
+
+namespace mastermind {
+
+    template<typename Item>
+    class basic_code_pattern_generator {
+    public:
+        basic_code_pattern_generator(const std::vector<Item>& code_set);
+        std::vector<Item> operator()(size_t pattern_size) const;
+    private:
+        std::vector<Item> m_code_set;
+        std::random_device rd{};
+        std::mt19937 g{ rd() };
+    };
+
+    template<typename Item>
+    basic_code_pattern_generator<Item>::basic_code_pattern_generator(const std::vector<Item>& code_set)
+        : m_code_set{ code_set } {
+    };
+
+    template<typename Item>
+    std::vector<Item> basic_code_pattern_generator<Item>::operator()(size_t pattern_size) const {
+        std::shuffle(m_code_set.begin(), m_code_set.end(), g);
+        return { m_code_set.begin(), m_code_set.begin() + pattern_size };
+    }
+
+}
